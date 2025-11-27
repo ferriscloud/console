256c4c82443049699ee00cec690bc30f0b983c9a
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2025-11-27 10:23:56 +0100

    feat: add dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,25 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './app.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-createRoot(document.getElementById('root')!).render(
+const container = document.getElementById('root') || (document.createElement('div') as HTMLElement)
+const root = createRoot(container)
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false
+    }
+  }
+})
+
+const render = (
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </StrictMode>
 )
+
+root.render(render)
